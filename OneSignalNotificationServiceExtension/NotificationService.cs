e2d86e46861df7b10800f22fc1ee6083e4dd1f62
AhmedFayez299 <93268385+AhmedFayez299@users.noreply.github.com>
2025-02-21 16:05:03 +0200

    update OnSignal

--- OneSignalNotificationServiceExtension/NotificationService.cs
+++ OneSignalNotificationServiceExtension/NotificationService.cs
@@ -1,5 +1,7 @@
-﻿using Foundation;
-using System;
+﻿using System;
+using Foundation;
+using OneSignalSDK.DotNet;
+using OneSignalSDK.DotNet.iOS;
 using UIKit;
 using UserNotifications;
 
@@ -10,6 +12,7 @@ namespace OneSignalNotificationServiceExtension
     {
         Action<UNNotificationContent> ContentHandler { get; set; }
         UNMutableNotificationContent BestAttemptContent { get; set; }
+        UNNotificationRequest ReceivedRequest { get; set; }
 
         protected NotificationService(IntPtr handle) : base(handle)
         {
@@ -18,19 +21,19 @@ namespace OneSignalNotificationServiceExtension
 
         public override void DidReceiveNotificationRequest(UNNotificationRequest request, Action<UNNotificationContent> contentHandler)
         {
+            ReceivedRequest = request;
             ContentHandler = contentHandler;
             BestAttemptContent = (UNMutableNotificationContent)request.Content.MutableCopy();
 
-            // Modify the notification content here...
-            BestAttemptContent.Title = string.Format("{0}[modified]", BestAttemptContent.Title);
-
-            ContentHandler(BestAttemptContent);
+            NotificationServiceExtension.DidReceiveNotificationExtensionRequest(request, BestAttemptContent, contentHandler);
         }
 
         public override void TimeWillExpire()
         {
             // Called just before the extension will be terminated by the system.
             // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+
+            NotificationServiceExtension.ServiceExtensionTimeWillExpireRequest(ReceivedRequest, BestAttemptContent);
 
             ContentHandler(BestAttemptContent);
         }
