55ce8505ef00fc4e7c6f4f8b5becb6f1a240591e
MR.Akl <boodyakl12@gmail.com>
2025-03-21 11:46:42 +0200

    update

--- FixProUs/Models/EmployeesCategoryModel.cs
+++ FixProUs/Models/EmployeesCategoryModel.cs
@@ -9,9 +9,9 @@ namespace FixProUs.Models
         public int Id { get; set; }
         public int? AccountId { get; set; }
         public int? BrancheId { get; set; }
-        public string CategoryName { get; set; }
-        public string Description { get; set; }
-        public string Notes { get; set; }
+        public string? CategoryName { get; set; }
+        public string? Description { get; set; }
+        public string? Notes { get; set; }
         public bool? Active { get; set; }
         public int? CreateUser { get; set; }
         public DateTime? CreateDate { get; set; }
